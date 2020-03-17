b87682e2dea8697b5cafc4d97efec67bb654a050
Aziz Ben Salem <azizbensalem1998@gmail.com>
2020-03-17 21:22:12 +0100

    Updates

---
+++ precache-manifest.7b750a7274c52c249a83614ec87a1c3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca6120bdee9af24d40b37606a174549d",
+    "url": "/clicnfest/index.html"
+  },
+  {
+    "revision": "f31d6b7106d902db4374",
+    "url": "/clicnfest/static/css/main.3d02b5d8.chunk.css"
+  },
+  {
+    "revision": "c7919687c87fcd845c0c",
+    "url": "/clicnfest/static/js/2.d6abcf29.chunk.js"
+  },
+  {
+    "revision": "e11d01fa865d2b39170e4ccde4b0b3d3",
+    "url": "/clicnfest/static/js/2.d6abcf29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f31d6b7106d902db4374",
+    "url": "/clicnfest/static/js/main.968e2df4.chunk.js"
+  },
+  {
+    "revision": "95e851fc650cb47d0744",
+    "url": "/clicnfest/static/js/runtime-main.2257920b.js"
+  },
+  {
+    "revision": "d8c8d1e43840ea9de70efd318fde9840",
+    "url": "/clicnfest/static/media/clicnfest.d8c8d1e4.PNG"
+  }
+]);
